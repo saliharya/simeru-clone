36821e574b6741c20219b9f8dcb125f9f184e377
saliharya <saliharyazinc@gmail.com>
2024-07-19 21:22:27 +0700

    configure next auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,13 +1,55 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials"
+
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    // ...add more providers here
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "Username" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        const users = [
+          { id: "1", username: "mhs", password: "mhs", role: "student" },
+          { id: "2", username: "dosen", password: "dosen", role: "lecturer" },
+          { id: "3", username: "admin", password: "admin", role: "admin" }
+        ]
+
+        const user = users.find(user =>
+          user.username === credentials.username && user.password === credentials.password
+        )
+
+        if (user) {
+          return { id: user.id, name: user.username, email: `${user.username}@example.com`, role: user.role }
+        } else {
+          return null
+        }
+      }
+    })
   ],
+  pages: {
+    signIn: '/auth/AuthPage',
+  },
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+        token.role = user.role
+      }
+      return token
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id
+        session.user.role = token.role
+      }
+      return session
+    }
+  }
 }
+
 export default NextAuth(authOptions)
