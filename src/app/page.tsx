fed646fbde4fa01183dc9576cb7026736d874240
saliharya <saliharyazinc@gmail.com>
2024-07-19 19:26:34 +0700

    refactoring code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,10 @@
 import React from 'react';
-import ScheduleIcon from '@/components/scheduleicon';
-import studyProgramIcon from '../../public/study_program_icon.png';
-import lecturerIcon from '../../public/lecturer_icon.png';
-import classIcon from '../../public/class_icon.png';
+import ScheduleMenuList from '@/components/schedulemenulist';
 
 export default function Home() {
   return (
-    <>
-      <ScheduleIcon
-        icon={studyProgramIcon}
-        name="Jadwal Program Studi"
-        route="/JadwalProdiPage"
-      />
-      <ScheduleIcon
-        icon={lecturerIcon}
-        name="Jadwal Dosen"
-        route="/JadwalDosenPage"
-      />
-      <ScheduleIcon
-        icon={classIcon}
-        name="Jadwal Ruang"
-        route="/JadwalRuangPage"
-      />
-    </>
+    <div>
+      <ScheduleMenuList />
+    </div>
   )
 }
---
+++ src/components/schedulemenulist/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import ScheduleIcon from '@/components/scheduleicon';
+import studyProgramIcon from '../../../public/study_program_icon.png';
+import lecturerIcon from '../../../public/lecturer_icon.png';
+import classIcon from '../../../public/class_icon.png';
+import { StaticImageData } from 'next/image';
+
+interface ScheduleItem {
+    icon: StaticImageData;
+    name: string;
+    route: string;
+}
+
+const scheduleItems: ScheduleItem[] = [
+    {
+        icon: studyProgramIcon,
+        name: 'Jadwal Program Studi',
+        route: '/JadwalProdiPage',
+    },
+    {
+        icon: lecturerIcon,
+        name: 'Jadwal Dosen',
+        route: '/JadwalDosenPage',
+    },
+    {
+        icon: classIcon,
+        name: 'Jadwal Ruang',
+        route: '/JadwalRuangPage',
+    },
+];
+
+const ScheduleMenuList: React.FC = () => {
+    return (
+        <div>
+            {scheduleItems.map((item, index) => (
+                <ScheduleIcon
+                    key={index}
+                    icon={item.icon}
+                    name={item.name}
+                    route={item.route}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default ScheduleMenuList;
