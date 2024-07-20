5a88374cbe6d3e8af53f9b2cc42d09d1478cfb85
saliharya <saliharyazinc@gmail.com>
2024-07-20 08:32:33 +0700

    add data jadwal pak dinan

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
-            <main className="flex-1 mt-28 mb-16"> {/* Flex-grow to take remaining space */}
+            <main className="flex-1 mt-28 mb-16">
                 <Component {...pageProps} />
             </main>
             <Footer />
