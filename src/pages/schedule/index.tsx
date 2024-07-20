49fddbda73e68e70d2b9331eb73fecdcd4440f79
saliharya <saliharyazinc@gmail.com>
2024-07-20 13:32:07 +0700

    center title

--- src/pages/schedule/index.tsx
+++ src/pages/schedule/index.tsx
@@ -12,7 +12,7 @@ export default function SchedulePage() {
 
     return (
         <div className="flex flex-col items-center min-h-screen bg-gray-100">
-            <h1 className="text-4xl font-bold mb-12 mt-16 text-gray-800">Sistem Informasi Manajemen Ruang</h1>
+            <h1 className="text-4xl font-bold mb-12 mt-16 text-gray-800 text-center">Sistem Informasi Manajemen Ruang</h1>
             <div className="container mx-auto p-6">
                 <ScheduleMenuList />
             </div>
