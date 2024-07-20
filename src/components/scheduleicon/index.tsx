45c5369a2356b24da5fa9e3a58ad448f602b6e27
saliharya <saliharyazinc@gmail.com>
2024-07-20 11:24:17 +0700

    change img to NextImage

--- src/components/scheduleicon/index.tsx
+++ src/components/scheduleicon/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StaticImageData } from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
 
 interface ScheduleIconProps {
@@ -10,15 +10,17 @@ interface ScheduleIconProps {
 
 const ScheduleIcon: React.FC<ScheduleIconProps> = ({ icon, name, route }) => {
     return (
-        <Link href={route}>
+        <Link href={route} passHref>
             <div className="flex flex-col items-center p-4 transition-transform transform hover:text-blue-500 rounded-lg">
-                <img
-                    src={icon.src}
-                    alt={name}
-                    width={200}
-                    height={200}
-                    className="object-contain transition-transform transform hover:scale-125"
-                />
+                <div className="relative w-48 h-48">
+                    <Image
+                        src={icon}
+                        alt={name}
+                        layout="fill"
+                        objectFit="contain"
+                        className="transition-transform transform hover:scale-125"
+                    />
+                </div>
                 <span className="mt-8 text-lg font-medium">{name}</span>
             </div>
         </Link>
